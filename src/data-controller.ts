127d7e30fa142a6bfa3da7250cad71fc106dff8f
Selmir Lelak <Lelaks@mail.gvsu.edu>
2024-03-01 15:02:21 -0500

    final test

--- src/data-controller.ts
+++ src/data-controller.ts
@@ -8,17 +8,24 @@ function generateProductHTML(product: Product): string {
               <span>${product.rating}/5</span><span>$${product.price}</span><span>stock ${product.stock}</span>
             </div>`;
 }
-
+// for returning html strings as single string
+// https://stackoverflow.com/questions/44062723/mapping-an-object-and-then-setting-innerhtml-on-the-page-results-in-commas-betwe
 function renderProducts(prods: Product[]): void {
-    // your code
+    const mainContainer = document.getElementById("main-container");
+    if (!mainContainer) return; //get rid of maincontainer can possibly be null error
+    mainContainer.innerHTML = ''; // Clear screen
+    const productsHTML = prods.map(generateProductHTML).join('');
+    mainContainer.innerHTML = productsHTML;
 }
 
 function getByCategory(category: string): void {
-    // your code
+    const filteredProducts = products.filter(product => product.category === category); //filter based on passed in catergory from on click
+    renderProducts(filteredProducts);
 }
 
 function getByRating(minRating: number): void {
-    // your code
+    const filteredProducts = products.filter(product => product.rating > minRating); //filter based on rating from on click
+    renderProducts(filteredProducts);
 }
 
 export { renderProducts, getByCategory, getByRating };
